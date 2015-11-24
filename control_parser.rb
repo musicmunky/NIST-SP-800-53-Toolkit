2ae5565a774966441fc3b7eb56db25d58d213308
musicmunky <musicmunky@gmail.com>
2015-11-24 10:04:56 -0500

    Update to make the parser much more flexible (and not store a password)

--- control_parser.rb
+++ control_parser.rb
@@ -3,8 +3,9 @@
 # This script processes the 800-53-controls.xml document provided by NIST
 # (https://nvd.nist.gov/static/feeds/xml/sp80053/rev4/800-53-controls.xml), and
 # parses various components into a MYSQL database.
-# Quick and dirty, but it works... More or less.
 
+# REQUIRED GEMS:
+require 'optparse'
 require 'nokogiri'
 require 'mysql2'
 require 'sequel'
@@ -37,36 +38,40 @@ def parse_control(path)
 		@family_id = @find_family.first[:id]
 	end
 
+	if @family_id > 0
 
-	@control_id = MYSQL[:controls].insert(
-		'family_id' => @family_id,
-		'family_name' => @family_name,
-		'number' => @cnumber,
-		'title' => @title,
-		'priority' => @priority,
-		'is_baseline_impact_low' => @baseline_impacts['LOW'],
-		'is_baseline_impact_moderate' => @baseline_impacts['MODERATE'],
-		'is_baseline_impact_high' => @baseline_impacts['HIGH'],
-		'is_withdrawn' => @withdrawn,
-		'is_enhancement' => 0,
-		"created_at" => Time.now.strftime("%Y-%m-%d %H:%M:%S"),
-		"updated_at" => Time.now.strftime("%Y-%m-%d %H:%M:%S")
-	)
+		@control_id = MYSQL[:controls].insert(
+			'family_id' => @family_id,
+			'family_name' => @family_name,
+			'number' => @cnumber,
+			'title' => @title,
+			'priority' => @priority,
+			'is_baseline_impact_low' => @baseline_impacts['LOW'],
+			'is_baseline_impact_moderate' => @baseline_impacts['MODERATE'],
+			'is_baseline_impact_high' => @baseline_impacts['HIGH'],
+			'is_withdrawn' => @withdrawn,
+			'is_enhancement' => 0,
+			"created_at" => Time.now.strftime("%Y-%m-%d %H:%M:%S"),
+			"updated_at" => Time.now.strftime("%Y-%m-%d %H:%M:%S")
+		)
 
-	if path.xpath("supplemental-guidance").length > 0
-		parse_supplemental_guidance(@control_id, path.xpath("supplemental-guidance"))
-	end
+		if path.xpath("supplemental-guidance").length > 0
+			parse_supplemental_guidance(@control_id, path.xpath("supplemental-guidance"))
+		end
 
-	if path.xpath("references").length > 0
-		parse_references(@control_id, path.xpath("references"))
-	end
+		if path.xpath("references").length > 0
+			parse_references(@control_id, path.xpath("references"))
+		end
 
-	if path.xpath("statement").length > 0
-		parse_statements(@control_id, path.xpath("statement"), @number) # Because stupid NIST schema!
-	end
+		if path.xpath("statement").length > 0
+			parse_statements(@control_id, path.xpath("statement"), @number) # Because stupid NIST schema!
+		end
 
-	if path.xpath("control-enhancements").length > 0
-		parse_control_enhancements(@control_id, path.xpath("control-enhancements"))
+		if path.xpath("control-enhancements").length > 0
+			parse_control_enhancements(@control_id, path.xpath("control-enhancements"))
+		end
+	else
+		puts "COULD NOT FIND FAMILY ID FOR CONTROL #{@title}"
 	end
 
 end
@@ -214,10 +219,12 @@ end
 
 
 def create_tables
+
+	# Keep sequel from treating tinyint 0/1 as boolean
 	MYSQL.convert_tinyint_to_bool = false
 
 	puts "creating FAMILIES table..."
-	MYSQL.create_table(:families) do
+	MYSQL.create_table!(:families) do
 		primary_key :id
 		text :name
 		text :acronym
@@ -228,7 +235,7 @@ def create_tables
 	puts "done\n"
 
 	puts "creating CONTROLS table..."
-	MYSQL.create_table(:controls) do
+	MYSQL.create_table!(:controls) do
 		primary_key :id
 		Integer :parent_id
 		Integer :family_id
@@ -247,7 +254,7 @@ def create_tables
 	puts "done\n"
 
 	puts "creating REFERENCES table..."
-	MYSQL.create_table(:references) do
+	MYSQL.create_table!(:references) do
 		primary_key :id
 		Integer :control_id
 		text :reference
@@ -258,7 +265,7 @@ def create_tables
 	puts "done\n"
 
 	puts "creating STATEMENTS table..."
-	MYSQL.create_table(:statements) do
+	MYSQL.create_table!(:statements) do
 		primary_key :id
 		Integer :control_id
 		String :number
@@ -270,7 +277,7 @@ def create_tables
 	puts "done\n"
 
 	puts "creating SUPPLEMENTS table..."
-	MYSQL.create_table(:supplements) do
+	MYSQL.create_table!(:supplements) do
 		primary_key :id
 		Integer :control_id
 		text :description
@@ -281,7 +288,7 @@ def create_tables
 	puts "done\n"
 
 	puts "creating WITHDRAWALS table..."
-	MYSQL.create_table(:withdrawals) do
+	MYSQL.create_table!(:withdrawals) do
 		primary_key :id
 		String :number
 		String :incorporated_into
@@ -294,21 +301,84 @@ end
 
 ######################################### Main ###################################################
 
+# For reference:
+# Sequel gem documentation: http://sequel.jeremyevans.net/documentation.html
 # https://coderwall.com/p/03r98q/using-the-nokogiri-gem-to-parse-nested-xml-data-in-ruby
 # http://stackoverflow.com/questions/11156781/xpath-in-nokogiri-returning-empty-array-whereas-i-am-expecting-to-have-result
 
-DEFAULT_FILE = '800-53-controls.xml'
-MYSQL = Sequel.connect(:adapter => 'mysql2', :host => "localhost", :database => "DBNAME", :username => "DBUSERNAME", :password=>'DBPASSWORD!')
+# NOTES: This script was written under the assumption that you're using a MySQL database.
+#		 If this is not the case, you'll have to tweak it a bit to use your specific adapter.
+#		 The default name is listed below - feel free to change it to the name of your db.
+#		 The script will drop/recreate all of the necessary tables for the control_freak
+#		 application, so if you make any schema changes to the app you'll need to also update
+#		 this script, otherwise any changes you've made the to database will be wiped out.
 
-@file = ARGV.shift || DEFAULT_FILE
-@doc = Nokogiri::XML(File.open(@file))
-@doc.remove_namespaces!
+# example usage:
+# ruby control_parser.rb -u root -p P4SSW0RD! -d control_freak_development
 
-create_tables()
+options = {}
+OptionParser.new do |opts|
+	opts.banner = "Usage: control_parser.rb [options]"
 
-puts "Beginning XML parsing now..."
-@doc.xpath("//control").each do |control|
-	parse_control(control)
+	opts.on("-h", "--help", "Script options:") do
+		puts opts
+		exit
+	end
+
+	opts.on("-u username", "--user username", String, "Set database user (REQUIRED)") do |u|
+		options[:username] = u
+	end
+
+	opts.on("-p password", "--password password", String, "Set database password (REQUIRED)") do |p|
+		options[:password] = p
+	end
+
+	opts.on("-f filename", "--file filename", String, "Filename to parse (defaults to '800-53-controls.xml')") do |f|
+		options[:filename] = f
+	end
+
+	opts.on("-o host", "--host hostname", String, "Database hostname (defaults to 'localhost')") do |h|
+		options[:hostname] = h
+	end
+
+	opts.on("-d database", "--database database", String, "Set the database for the parser (defaults to 'control_freak_development')") do |d|
+		options[:database] = d
+	end
+
+end.parse!
+
+dbfile = options[:filename] ? options[:filename] : "800-53-controls.xml"
+dbhost = options[:hostname] ? options[:hostname] : "localhost"
+dbname = options[:database] ? options[:database] : "control_freak_development"
+dbuser = options[:username] ? options[:username] : ""
+dbpass = options[:password] ? options[:password] : ""
+
+if dbuser == "" or dbpass == ""
+	puts "Please enter the username and password for your database!\n\n"
+	exit(0)
 end
-puts "Completed XML parsing - please verify database is correct\n"
 
+begin
+	puts "Checking for database - creating one if it doen't exist..."
+	client = Mysql2::Client.new(:host => dbhost, :username => dbuser, :password => dbpass)
+	client.query("CREATE DATABASE IF NOT EXISTS #{dbname};")
+	client.close
+	puts "done"
+
+	MYSQL = Sequel.connect(:adapter => 'mysql2', :host => dbhost, :database => dbname, :username => dbuser, :password => dbpass, :test => true)
+
+	@doc = Nokogiri::XML(File.open(dbfile))
+	@doc.remove_namespaces!
+
+	create_tables()
+
+	puts "Beginning XML parsing now..."
+	@doc.xpath("//control").each do |control|
+		parse_control(control)
+	end
+	puts "Completed XML parsing - please verify database is correct\n"
+	exit(0)
+
+rescue => error
+	puts "ERROR PROCESSING: #{error.message}\n\nFULL ERROR:\n#{error.backtrace}"
+end
